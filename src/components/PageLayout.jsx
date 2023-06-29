03e49ec664d554b37c287c910085212fc3b45038
Prashant prabhakar <prashantp.cs.21@nitj.ac.in>
2023-06-30 00:17:04 +0530

    refactored Footer CSS , removed the extra file :footer

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from "./Header"
 import Navbar from './Navbar'
+import Footer from './Footer'
 function PageLayout() {
   return (
     <>
       <Header />
       <Navbar />
       <Outlet />
+      <Footer/>
     </>
 
   )
