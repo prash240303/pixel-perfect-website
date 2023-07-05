c4517c3f328650c1c0ad361e0f6cf443dda864e8
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-07-05 15:10:18 +0545

    fixed:on clicking any option in navbar in dropdown list(mobile view) , it still remains

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {
-  Navbar,
-  MobileNav,
-  Typography,
-  Button,
-  IconButton,
-} from "@material-tailwind/react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, MobileNav, Typography, IconButton } from "@material-tailwind/react";
+
 function Navbaar() {
   const [openNav, setOpenNav] = React.useState(false);
  
@@ -16,7 +11,11 @@ function Navbaar() {
       () => window.innerWidth >= 960 && setOpenNav(false)
     );
   }, []);
- 
+
+  const closeNav = () => {
+    setOpenNav(false);
+  };
+
   const navList = (
     <ul className="mb-4 mt-2 flex flex-col lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:flex-wrap ">
       <Typography
@@ -24,124 +23,133 @@ function Navbaar() {
         variant="small"
         color="blue-gray"
         className="p-1 font-bold text-lg uppercase text-black cursor-pointer"
+        onClick={closeNav}
       >
-        <Link  to="/" className="mr-10  hover:text-gray-700 ">Home</Link>
+        <Link to="/" className="mr-10 hover:text-gray-700">Home</Link>
       </Typography>
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
         className="p-1 font-bold text-lg uppercase text-black cursor-pointer"
+        onClick={closeNav}
       >
-        <Link to="/" className="mr-10  hover:text-gray-700 ">paper themes</Link>
+        <Link to="/paper-themes" className="mr-10 hover:text-gray-700">Paper Themes</Link>
       </Typography>
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
         className="p-1 font-bold text-lg uppercase text-black cursor-pointer"
+        onClick={closeNav}
       >
-        <Link to="/speaker" className="mr-10  hover:text-gray-700 ">speakers</Link>
+        <Link to="/speakers" className="mr-10 hover:text-gray-700">Speakers</Link>
       </Typography>
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
         className="p-1 font-bold text-lg uppercase text-black cursor-pointer"
+        onClick={closeNav}
       >
-        <Link to="/committee" className="mr-10  hover:text-gray-700 ">committee</Link>
+        <Link to="/committee" className="mr-10 hover:text-gray-700">Committee</Link>
       </Typography>
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
         className="p-1 font-bold text-lg uppercase text-black cursor-pointer"
+        onClick={closeNav}
       >
-        <Link to="/" className="mr-10 hover:text-gray-700 ">sponsors</Link>
+        <Link to="/sponsors" className="mr-10 hover:text-gray-700">Sponsors</Link>
       </Typography>
       <Typography
         as="li"
         variant="small"
         color="black"
         className="p-1 font-bold text-lg uppercase text-black cursor-pointer "
+        onClick={closeNav}
       >
-        <Link to="/" className="mr-10  hover:text-gray-700 ">awards</Link>
+        <Link to="/awards" className="mr-10 hover:text-gray-700">Awards</Link>
       </Typography>
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
         className="p-1 font-bold text-lg uppercase text-black cursor-pointer"
+        onClick={closeNav}
       >
-        <Link to="/" className="mr-10 hover:text-gray-700 ">registrations</Link>
+        <Link to="/registrations" className="mr-10 hover:text-gray-700">Registrations</Link>
       </Typography>
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
         className="p-1 font-bold text-lg uppercase text-black cursor-pointer"
+        onClick={closeNav}
       >
-        <Link to="/contact" className="mr-10 hover:text-gray-700 ">contact </Link>
+        <Link to="/contact" className="mr-10 hover:text-gray-700">Contact</Link>
       </Typography>
     </ul>
   );
+
+  const toggleNav = () => {
+    setOpenNav(!openNav);
+  };
+
   return (
     <>
-    <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-4 lg:py-4">
-      <div className="flex items-center justify-end text-blue-gray-900">
-        
-        <div className="flex items-center gap-4">
-          <div className="mr-4 hidden lg:block">{navList}</div>
-          
-          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 color text-inherit text-black hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-            color='black'
-            ripple={false}
-            onClick={() => setOpenNav(!openNav)}
-          >
-            {openNav ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </IconButton>
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-4 lg:py-4">
+        <div className="flex items-center justify-end text-blue-gray-900">
+          <div className="flex items-center gap-4">
+            <div className="mr-4 hidden lg:block">{navList}</div>
+
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 color text-inherit text-black hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              color='black'
+              ripple={false}
+              onClick={toggleNav}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
         </div>
-      </div>
-      <MobileNav open={openNav}>
-        {navList}
-        
-      </MobileNav>
-    </Navbar>
+        <MobileNav open={openNav}>
+          <div className="max-w-full" onClick={closeNav}>{navList}</div>
+        </MobileNav>
+      </Navbar>
     </>
-  )
+  );
 }
 
-export default Navbaar
-
-
+export default Navbaar;
