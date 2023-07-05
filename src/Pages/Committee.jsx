ce461bd14f2b62a3a392a050375895075aab554c
nishchay-veer <nishchayveer19@gmail.com>
2023-07-05 13:32:36 +0530

    overflow-x: clip

--- src/Pages/Committee.jsx
+++ src/Pages/Committee.jsx
@@ -17,7 +17,7 @@ function Committee() {
           <div className='flex   flex-col  items-start   text-[4rem] w-[400px] font-custom font-bold '>
             ORGANIZING COMMITTEE
           </div>
-          <div className=' committee-container  flex flex-col  w-full overflow-hidden px-6 border-l border-black'>
+          <div className=' committee-container  flex flex-col  w-full overflow-hidden md:px-6 border-l border-black'>
            {(
             <div id='cards-wrapper' className='flex w-full  item flex-col gap-6  items-start  align-top '>
               {data.length > 0 ? (
