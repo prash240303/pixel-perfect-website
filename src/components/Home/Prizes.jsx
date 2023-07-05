b20917c00e539a80f84fa7adcc694584174fa74d
Prashant prabhakar <prashantp.cs.21@nitj.ac.in>
2023-07-05 21:31:03 +0530

    responsiveness prizes section

--- src/components/Home/Prizes.jsx
+++ src/components/Home/Prizes.jsx
@@ -5,17 +5,16 @@ function Prizes() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const endPoint = "/awards/conference/647f315f62cdb3a26174fc38";
-  
     useEffect(() => {
         fetchAPI(setData, setLoading, endPoint);
     }, []);
     return (
-        <section className="prizes-section   px-6 py-6 pb-6 w-full items-start  border-t  border-black justify-start">
+        <section className="prizes-section   px-6  pb-6 w-full items-start  border-t  border-black justify-start">
             <div className='main-wrapper w-full flex flex-col gap-8  px-6 border-x  border-black '>
                 <div className=' pr-6  text-7xl'>
                     <div className='font-custom font-bold leading-normal'>AWARDS</div>
                 </div>
-                {<div className='flex flex-col flex-wrap md:flex-row gap-12 justify-between items-center'>
+                {<div className='flex flex-col flex-wrap md:flex-row justify-center  items-start'>
                     {data.length > 0 ? (
                         data.map((item) => {
                             return <PrizeCard key={item.id} item={item} />;
--- src/components/Prizes/PrizeCard.jsx
+++ src/components/Prizes/PrizeCard.jsx
@@ -1,32 +1,37 @@
 import React, { useState } from 'react';
 
 function PrizeCard({ item }) {
-    console.log("prize data: ", item)
-    const [showMore, setShowMore] = useState(false);
+  console.log('prize data: ', item);
+  const [showMore, setShowMore] = useState(false);
 
   function handleReadmore() {
     setShowMore(!showMore);
   }
-    return (
-        <div className='flex  w-[360px] h-[440px] px-6 py-6 bg-black rounded-[20px]'>
-            <div className='flex flex-col gap-4 h-auto place-items-center px-4 border-x text-white border-white'>
-                <div className='flex flex-row  font-bold font-custom text-5xl  items-center  text-center'>{item.title1}</div>
-                <div className='flex flex-wrap font-bold py-1 text-3xl px-2 items-center text-center w-45 rounded-[12px] bg-white text-black'>{item.title2}</div>
-                <div className='flex flex-wrap font-normal text-center'>
-                    <p>
-                        {showMore || item.description.length <= 130
-                            ? item.description
-                            : `${item.description.slice(0, 130)}...`}
-                    </p>
-                    {item.description.length > 130 && (
-                        <button onClick={handleReadmore} className="font-semibold">
-                            {showMore ? 'Read less' : 'Read more'}
-                        </button>
-                    )}
-                </div>
-            </div>
+
+  return (
+    <div className="flex flex-col  mt-6 md:flex-row md:w-[300px] mx-6 px-2 md:py-6 md:h-max bg-black rounded-[20px] flex-grow">
+      <div className="flex flex-col gap-4 place-items-center px-4 border-x text-white border-white w-full h-full">
+        <div className="flex flex-row font-bold text-center tracking-tight font-custom text-5xl leading-snug items-center justify-center">
+          {item.title1}
         </div>
-    )
+        <div className="flex flex-wrap font-bold py-1 text-3xl px-2 items-center justify-center rounded-[12px] bg-white text-black">
+          {item.title2}
+        </div>
+        <div className="flex flex-wrap font-normal text-left flex-grow">
+          <p className="h-full">
+            {showMore || item.description.length <= 130
+              ? item.description
+              : `${item.description.slice(0, 130)}...`}
+          </p>
+          {item.description.length > 130 && (
+            <button onClick={handleReadmore} className="font-semibold">
+              {showMore ? 'Read less' : 'Read more'}
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default PrizeCard
+export default PrizeCard;
