ababe8789f311d90981456f45a8a973ca2d809da
Prashant prabhakar <prashantp.cs.21@nitj.ac.in>
2023-07-06 13:32:06 +0530

    fix : Navbar border

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -132,7 +132,7 @@ function Navbaar() {
 
   return (
     <>
-      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-4 lg:py-4">
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none border-b-black py-2 px-4 lg:px-4 lg:py-4">
         <div className="flex items-center justify-end text-blue-gray-900">
           <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">{navList}</div>
