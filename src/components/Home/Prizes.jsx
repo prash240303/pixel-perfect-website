29b02ca5a9af17caf831f31e9bb5620e841abbd1
Prashant prabhakar <prashantp.cs.21@nitj.ac.in>
2023-07-06 13:29:42 +0530

    fixed Prizes section padding

--- src/components/Home/Prizes.jsx
+++ src/components/Home/Prizes.jsx
@@ -11,30 +11,31 @@ function Prizes() {
     return (
         <section className="prizes-section  py-6 px-6  pb-6 w-full items-start  border-t  border-black justify-start">
             <div className='main-wrapper w-full flex flex-col gap-8  px-6 border-x  border-black '>
-                <div className=' pr-6  text-7xl'>
+                <div className='  text-7xl'>
                     <div className='font-custom font-bold leading-normal'>AWARDS</div>
                 </div>
-                {<div className='flex flex-col justify-between md:flex-row'>
-                    {data.length > 0 ? (
-                        data.map((item) => {
-                            return <PrizeCard key={item.id} item={item} />;
-                        })
-                    ) : (
-                        <>
-                            <PrizeCard
-                                single
-                                item={{
-                                    id: "default",
-                                    title1: "loading",
-                                    title2: "loading",
-                                    description: "loading",
-                                }}
-                            />
-                        </>
-                    )}
-                </div>
-                }
             </div>
+
+            {<div className='flex flex-col justify-between md:flex-row border-x border-black'>
+                {data.length > 0 ? (
+                    data.map((item) => {
+                        return <PrizeCard key={item.id} item={item} />;
+                    })
+                ) : (
+                    <>
+                        <PrizeCard
+                            single
+                            item={{
+                                id: "default",
+                                title1: "loading",
+                                title2: "loading",
+                                description: "loading",
+                            }}
+                        />
+                    </>
+                )}
+            </div>
+            }
         </section>
 
     )
