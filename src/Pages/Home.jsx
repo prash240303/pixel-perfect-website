8b01c70d89503191ed051e0d84f4feb30d913bb1
Prashant prabhakar <prashantp.cs.21@nitj.ac.in>
2023-07-31 01:18:56 +0530

    added the loading icon back

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,15 @@
-import '../styles/Home.css';
-import Hero from '../components/Home/Hero';
-import Announcement from '../components/Home/Announcement';
-import About from '../components/Home/About';
-import Dates from '../components/Home/Dates';
-import Sponsers from '../components/Home/Sponsers';
-import Prizes from '../components/Home/Prizes';
-import fetchAPI from '../utils/fetchAPI';
-import LoadingIcon from '../components/LoadingIcon';
-import { React, useEffect, useState } from 'react';
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-
+import "../styles/Home.css";
+import Hero from "../components/Home/Hero";
+import Announcement from "../components/Home/Announcement";
+import About from "../components/Home/About";
+import Dates from "../components/Home/Dates";
+import Sponsers from "../components/Home/Sponsers";
+import Prizes from "../components/Home/Prizes";
+import fetchAPI from "../utils/fetchAPI";
+import LoadingIcon from "../components/LoadingIcon";
+import { React, useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function Home() {
   const [heroData, setHeroData] = useState({});
@@ -26,23 +25,40 @@ function Home() {
   const announcementEndPoint = "/announcement/conf/647f315f62cdb3a26174fc38";
 
   useEffect(() => {
-    fetchAPI(setHeroData, setLoading, heroEndPoint);
-    fetchAPI(setDatesData, setLoading, datesEndPoint);
-    fetchAPI(setSponsersData, setLoading, sponsersEndPoint);
-    fetchAPI(setImageData, setLoading, imageEndPoint);
-    fetchAPI(setAnnoucementData, setLoading, announcementEndPoint)
+    // Define a helper function to fetch data and update state
+    const fetchData = async () => {
+      // Introduce a 3-second delay using setTimeout
+      setTimeout(async () => {
+        await Promise.all([
+          fetchAPI(setHeroData, setLoading, heroEndPoint),
+          fetchAPI(setDatesData, setLoading, datesEndPoint),
+          fetchAPI(setSponsersData, setLoading, sponsersEndPoint),
+          fetchAPI(setImageData, setLoading, imageEndPoint),
+          fetchAPI(setAnnoucementData, setLoading, announcementEndPoint),
+        ]);
+      }, 3000); // 3 seconds delay
+    };
+
+    fetchData(); // Call the helper function to fetch data and update state
   }, []);
 
-  const heroImage = imageData.find((image) => image.name === "WIE volunteers") || {};
+  const heroImage =
+    imageData.find((image) => image.name === "WIE volunteers") || {};
   return (
     <>
       <main className="flex flex-col mt-6 w-full max-w-8xl mx-auto">
         {loading ? (
           // <LoadingIcon />
-          <Skeleton
-          count={5}
-          
-          />
+          // <>
+          //   <div className="flex flex-col gap-12">
+          //     <Skeleton count={1} className="mx-12 h-10 mb-6" />
+          //     <div className="flex gap-6 w-full border border-black">
+          //       <Skeleton count={3} className="mx-12 h-10 w-fill mb-6" />
+          //       <Skeleton count={3} className="mx-12 h-10 w-fll mb-6"/>
+          //     </div>
+          //   </div>
+          // </>
+          <LoadingIcon/>
         ) : (
           <>
             <Hero data={heroData} image={heroImage} />
